8359bdcf6fac75ffbcfd039767a4f1caed71bda5
mesmerlord <76161333+mesmerlord@users.noreply.github.com>
2021-09-15 01:24:25 +0200

    to fix task new_novel exception handling

--- wuxiaworld/novels/tasks.py
+++ wuxiaworld/novels/tasks.py
@@ -197,7 +197,7 @@ def add_novels():
         except Exception as e:
             print(f"Book {x['Book Name']} , author {x['Book Author']} already exists")
         
-        novel, _ = Novel.objects.get_or_create(slug = slugify(x['Book Name'], author = author),
+        novel, _ = Novel.objects.get_or_create(slug = slugify(x['Book Name']), author = author,
                     defaults = {'slug': slugify(x['Book Name']), 'name' : x['Book Name'], 'image' : x['Book Image'], 'imageThumb' : x['thumbnail'],
                     'linkNU' : x['Book URL'], 'description' : x['Description'], 'numOfChaps' : int(x['Book Chapters'].strip().split(" ")[0]),
                     'numOfTranslatedChaps' : 0, 'novelStatus' : False , 'scrapeLink' : x['novelLink'], 'repeatScrape' : True})
