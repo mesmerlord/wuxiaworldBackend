2c0bdb2edfdabb99a8ca8e724707d5ddadc2ea42
mesmerlord <76161333+mesmerlord@users.noreply.github.com>
2021-10-23 17:33:57 +0200

    making wuxianovels have frontend

--- wuxiaworld/novels/urls.py
+++ wuxiaworld/novels/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path,re_path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from wuxiaworld.novels.views import (deleteDuplicate, deleteUnordered, addNovels,siteMap)
+from wuxiaworld.novels.views import (GoogleLogin)
 
 urlpatterns = [
     # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
