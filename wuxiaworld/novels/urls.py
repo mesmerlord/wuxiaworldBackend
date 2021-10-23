2f5dabece18e49032062fd9f1d7f476c5ac9fcff
mesmerlord <76161333+mesmerlord@users.noreply.github.com>
2021-10-23 17:08:17 +0200

    making wuxianovels have frontend

--- wuxiaworld/novels/urls.py
+++ wuxiaworld/novels/urls.py
@@ -34,6 +34,8 @@ if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [path(r'.*', home)]
 
+print(urlpatterns)
+
 if settings.DEBUG:
 
     urlpatterns += [
