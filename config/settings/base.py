b77ad2d7fede119138398a1e7aeffb2e80c53f6f
mesmerlord <76161333+mesmerlord@users.noreply.github.com>
2021-10-28 21:14:25 +0200

    fixing celery tasks

--- config/settings/base.py
+++ config/settings/base.py
@@ -255,7 +255,8 @@ LOGGING = {
 
 # Celery
 # ------------------------------------------------------------------------------
-CELERY_ENABLE_UTC = False
+CELERY_ENABLE_UTC = True
+CELERY_TIMEZONE = 'UTC'
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
 CELERY_BROKER_URL = env("CELERY_BROKER_URL")
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
